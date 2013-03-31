3d19fae2ee7a88c8511703e9d16e4c5ffe9210a9
xiaopan <xiaopan@xiaopan-pc>
2013-03-31 23:33:24 +0800

    优化

--- src/me/xiaopan/lifespirit/widget/Preference.java
+++ src/me/xiaopan/lifespirit/widget/Preference.java
@@ -1,36 +1,55 @@
 package me.xiaopan.lifespirit.widget;
 
+import me.xiaopan.javalibrary.util.StringUtils;
 import me.xiaopan.lifespirit.R;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Preference extends LinearLayout {
-	private TextView title;
-	private TextView intro;
+	private TextView titleText;
+	private TextView introText;
 	
 	public Preference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		setOrientation(LinearLayout.VERTICAL);
-		int padding = (int) getContext().getResources().getDimension(R.dimen.base_textSize_large);
-		setPadding(padding, padding, padding, padding);
+		setGravity(Gravity.CENTER_VERTICAL);
+		
+		LinearLayout linearLayout = new LinearLayout(getContext());
+		linearLayout.setGravity(Gravity.CENTER_VERTICAL);
+		titleText = new TextView(getContext());
+		titleText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getContext().getResources().getDimension(R.dimen.base_textSize_default));
+		titleText.setTextColor(getContext().getResources().getColor(R.color.base_black));
+		linearLayout.addView(titleText);
+		introText = new TextView(context);
+		introText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getContext().getResources().getDimension(R.dimen.base_textSize_small));
+		introText.setTextColor(getContext().getResources().getColor(R.color.base_gray_dark));
+		linearLayout.addView(introText);
+		addView(linearLayout, new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.FILL_PARENT, 1));
+		
+		ImageView imageView = new ImageView(getContext());
+		imageView.setImageResource(R.drawable.ic_arrow_right);
+		addView(imageView);
 		
 		TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.Preference);
-		
-		title = new TextView(getContext());
-		title.setTextSize(TypedValue.COMPLEX_UNIT_PX, getContext().getResources().getDimension(R.dimen.base_textSize_default));
-		title.setTextColor(getContext().getResources().getColor(R.color.base_black));
-		title.setText(typedArray.getString(R.styleable.Preference_title));
-		
-		intro = new TextView(context);
-		intro.setText(typedArray.getString(R.styleable.Preference_intro));
-		
+		updateTitle(typedArray.getString(R.styleable.Preference_title));
+		updateIntro(typedArray.getString(R.styleable.Preference_intro));
 		typedArray.recycle();
-		
-		addView(title);
-		addView(intro);
+	}
+	
+	public void updateTitle(String title){
+		titleText.setText(title);
+	}
+	
+	public void updateIntro(String intro){
+		introText.setText(intro);
+		if(!StringUtils.isNotNullAndEmpty(intro)){
+			introText.setVisibility(View.GONE);
+		}
 	}
 }
