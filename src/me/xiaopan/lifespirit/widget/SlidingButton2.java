cd5b1b1f9177043de3f8d2773db2bdef8c4cf9c5
ixiaopan <i@xiaopan.me>
2013-04-11 12:03:50 +0800

    完成SlidingButton

--- src/me/xiaopan/lifespirit/widget/SlidingButton2.java
+++ src/me/xiaopan/lifespirit/widget/SlidingButton2.java
@@ -1,6 +1,5 @@
 package me.xiaopan.lifespirit.widget;
 
-import me.xiaopan.androidlibrary.util.Logger;
 import me.xiaopan.lifespirit2.R;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -15,9 +14,13 @@ import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Scroller;
 
 @SuppressLint("DrawAllocation")
 public class SlidingButton2 extends View implements OnGestureListener, OnDoubleTapListener{
+	private static final int DURATION = 300;
+	private static final int MIN_ROLLING_DISTANCE = 30;//滚动最小生效距离
 	private GestureDetector gestureDetector;
 	private Bitmap maskBitmap;
 	private Bitmap frameBitmap;
@@ -25,6 +28,12 @@ public class SlidingButton2 extends View implements OnGestureListener, OnDoubleT
 	private Bitmap sliderBitmap;
 	private Paint paint;
 	private boolean on;
+	private int left;
+	private int onLeft;
+	private int offLeft;
+	private Scroller scroller;
+	private int scrollDistanceCount;
+	private boolean needHandle;//当在一组时件中发生了滚动操作时，在弹起或者取消的时候就需要根据滚动的距离来切换状态或者回滚
 
 	public SlidingButton2(Context context) {
 		super(context);
@@ -36,6 +45,7 @@ public class SlidingButton2 extends View implements OnGestureListener, OnDoubleT
 		sliderBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.btn_sliding_slilder_normal);
 		paint = new Paint();
 		paint.setFilterBitmap(false);
+		scroller = new Scroller(getContext(), new AccelerateDecelerateInterpolator());
 	}
 	
 	@Override
@@ -44,7 +54,7 @@ public class SlidingButton2 extends View implements OnGestureListener, OnDoubleT
         canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.MATRIX_SAVE_FLAG | Canvas.CLIP_SAVE_FLAG | Canvas.HAS_ALPHA_LAYER_SAVE_FLAG | Canvas.FULL_COLOR_LAYER_SAVE_FLAG | Canvas.CLIP_TO_LAYER_SAVE_FLAG);
         
         //绘制背景层
-        canvas.drawBitmap(backgroundBitmap, isOn()?(0-(backgroundBitmap.getWidth() - getWidth())):0, 0, paint);
+        canvas.drawBitmap(backgroundBitmap, left, 0, paint);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
         canvas.drawBitmap(maskBitmap, 0, 0, paint);
         paint.setXfermode(null);
@@ -53,7 +63,7 @@ public class SlidingButton2 extends View implements OnGestureListener, OnDoubleT
         canvas.drawBitmap(frameBitmap, 0, 0, paint);
 
         //绘制按钮层
-        canvas.drawBitmap(sliderBitmap, isOn()?(0-(backgroundBitmap.getWidth() - getWidth())):0, 0, paint);
+        canvas.drawBitmap(sliderBitmap, left, 0, paint);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
         canvas.drawBitmap(maskBitmap, 0, 0, paint);
         paint.setXfermode(null);
@@ -99,63 +109,88 @@ public class SlidingButton2 extends View implements OnGestureListener, OnDoubleT
 		}
 		
 		setMeasuredDimension(realWidthSize, realHeightSize);
+		
+		onLeft = -1 * (backgroundBitmap.getWidth() - getWidth());
 	}
 	
 	@Override
+	public void computeScroll() {
+		if(scroller.computeScrollOffset()){
+			left = scroller.getCurrX();
+			invalidate();
+		}
+	}
+
+	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		return gestureDetector.onTouchEvent(event);
+		gestureDetector.onTouchEvent(event);
+		//如果允许处理,并且当前事件使弹起或者取消
+		if(needHandle && (event.getAction() == MotionEvent.ACTION_CANCEL || event.getAction() == MotionEvent.ACTION_UP)){
+			//如果本次滚动的距离超过的最小生效距离，就切换状态，否则就回滚
+			if(Math.abs(scrollDistanceCount) >= MIN_ROLLING_DISTANCE){
+				setState(scrollDistanceCount > 0, left);
+			}else{
+				setState(isOn(), left);
+			}
+			needHandle = false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean onDown(MotionEvent e) {
-		Logger.w("按下：X="+e.getX()+"；Y="+e.getY());
+		scrollDistanceCount = 0;
+		needHandle = false;
 		return true;
 	}
 
 	@Override
 	public void onShowPress(MotionEvent e) {
-		Logger.w("ShowPress：X="+e.getX()+"；Y="+e.getY());
 	}
 
 	@Override
 	public boolean onSingleTapUp(MotionEvent e) {
-		Logger.w("SingleTapUp：X="+e.getX()+"；Y="+e.getY());
-		setOn(!isOn());
+		switchState();
 		return true;
 	}
 
 	@Override
 	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-		Logger.w("Scroll：distanceX="+distanceX+"；distanceY="+distanceY);
+		needHandle = true;
+		scrollDistanceCount += distanceX;
+		left -= distanceX;
+		if(left >= offLeft){
+			left = offLeft;
+		}else if(left <= onLeft){
+			left = onLeft;
+		}
+		invalidate();
 		return true;
 	}
 
 	@Override
 	public void onLongPress(MotionEvent e) {
-		Logger.w("LongPress：X="+e.getX()+"；Y="+e.getY());
 	}
 
 	@Override
 	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-		Logger.w("Fling：velocityX="+velocityX+"；velocityY="+velocityY);
+		needHandle = false;
+		setState(e2.getX() < e1.getX(), left);
 		return true;
 	}
 
 	@Override
 	public boolean onSingleTapConfirmed(MotionEvent e) {
-		Logger.w("SingleTapConfirmed：X="+e.getX()+"；Y="+e.getY());
 		return true;
    }
 
    @Override
    public boolean onDoubleTap(MotionEvent e) {
-        Logger.w("DoubleTap：X="+e.getX()+"；Y="+e.getY());
         return true;
    }
 
    @Override
    public boolean onDoubleTapEvent(MotionEvent e) {
-        Logger.w("DoubleTapEvent：X="+e.getX()+"；Y="+e.getY());
         return true;
    }
 
@@ -165,6 +200,47 @@ public class SlidingButton2 extends View implements OnGestureListener, OnDoubleT
 	
 	public void setOn(boolean on) {
 		this.on = on;
-		invalidate();
+	}
+	
+	/**
+	 * 滚动
+	 * @param startX 开始X坐标
+	 * @param endX 结束Y坐标
+	 */
+	private void scroll(int startX, int endX){
+		if(startX != endX){
+			scroller.startScroll(startX, 0, endX - startX, 0, DURATION);
+			invalidate();
+		}
+	}
+	
+	/**
+	 * 设置状态
+	 * @param on 开启还是关闭
+	 * @param startX 开始滚动的位置
+	 */
+	private void setState(boolean on, int startX){
+		setOn(on);
+		//如果是要开启
+		if(isOn()){
+			scroll(startX, onLeft);
+		}else{
+			scroll(startX, offLeft);
+		}
+	}
+	
+	/**
+	 * 设置状态
+	 * @param on 开启还是关闭
+	 */
+	private void setState(boolean on){
+		setState(on, on?offLeft:onLeft);
+	}
+	
+	/**
+	 * 切换状态
+	 */
+	private void switchState(){
+		setState(!isOn());
 	}
 }
